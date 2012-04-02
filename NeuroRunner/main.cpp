864b8c04f477b969afcfff3268f4b01842d9b276
Nicolas Luck <nicolas@lucksus.eu>
2012-04-02 19:43:56 +0200

    NeuroRunner interprets command line arguments as script code.

--- NeuroRunner/main.cpp
+++ NeuroRunner/main.cpp
@@ -1,12 +1,28 @@
 #include <QtCore/QCoreApplication>
 #include "application.h"
+#include "controller.h"
+#include <QtCore/QFile>
+#include <iostream>
+#include "scriptengine.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     a.processEvents();
+
     Application::instance().init();
-    Application::instance().do_script_console();
+
+    if(argc > 1){
+        std::cout << "Interpreting command line arguments..." << std::endl;
+        for(int i=1;i<argc;i++){
+            std::cout << argv[i] << std::endl;
+            Controller::instance().script_engine()->evaluate(argv[i]);
+        }
+    }else{
+        Application::instance().do_script_console();
+    }
+
+    std::cout << "Exiting..." << std::endl;
     Application::instance().destroy();
     return Application::instance().return_value();
 }
